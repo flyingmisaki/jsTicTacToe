9554f77c2f9e3510107bd89014b706e0c538de03
unknown <pierrick@live.co.uk>
2020-11-09 19:17:33 +0000

    Code refactor, board is now any size, added winner check on rows

--- classes/board.js
+++ classes/board.js
@@ -1,18 +1,50 @@
 // Represents a game board
 class Board {
-    constructor(state = [
-                            ["", "", ""], 
-                            ["", "", ""],
-                            ["", "", ""]
-                        ]) {
-        this.state = state
+    constructor(width, height) {
+        this.width = width
+        this.height = height
+        this.clear()
+    }
+    // Returns winner if there is one
+    getWinner() {
+        return this.getWinnerFromRows()
     }
 
-    // Prints the board as 3 rows and 2 dividers
+    // Checks each row for a winner. Returns the winner if there is one
+    getWinnerFromRows() {
+        for (let y = 0; y < this.height; y++) {
+            const row = this.state[y]
+            const winner = this.getWinnerFromRow(row)
+            if (winner != null) {
+                return winner
+            }
+        }
+        return null
+    }
+
+    // Returns winner (x, o, null)
+    getWinnerFromRow(row) {
+        const counters = ["x", "o"]
+        let winner = null
+        counters.forEach(counter => {
+            // Check if every cell in a row contains the current counter
+            if (row.every(cell => cell === counter)) {
+                winner = counter
+            }
+        }) 
+        return winner
+    }
+    
+    // Clears board
+    clear() {
+        this.state = new Array(this.width).fill(new Array(this.height).fill(''))
+    }
+
+    // Prints the board as rows and dividers
     print() {
         this.state.forEach((row, index) => {
             this.printRow(row)
-            if (index < 2) {
+            if (index < this.width - 1) {
                 this.printDivider()
             }
         })
@@ -21,14 +53,14 @@ class Board {
     // Prints a row
     printRow(row) {
         let rowString = ""
-        // adds the contents of the row to rowString
+        // Adds the contents of the row to rowString
         row.forEach((cell, rowIndex) => {
             rowString += " " + cell + " "
 
             if (cell == "") {
                 rowString += " "
             }
-            if (rowIndex < 2) {
+            if (rowIndex < this.width - 1) {
                 rowString += "|"
             } 
         })
@@ -37,7 +69,8 @@ class Board {
     
     // Prints a horizontal divider
     printDivider() {
-        console.log('\u2015\u2015\u2015 \u2015\u2015\u2015 \u2015\u2015\u2015')
+        const divider = '\u2015\u2015\u2015 '
+        console.log(`${divider.repeat(this.width)}`)
     }
 
     // Returns true if every cell of the board is empty
--- gameTicTacToe.js
+++ gameTicTacToe.js
@@ -1,7 +1,13 @@
-// Impor Board class from seperate file
-// import Board from "./classes/board.js"
+// Import Board from "./classes/board.js"
 const Board = require("./classes/board.js")
 
-const board = new Board([["o", "o", "x"], ["", "", ""], ["x", "", "o"]])
+const board = new Board(4, 4)
+
+board.state = [["x", "x", "o", "x"], 
+               ["x", "o", "o", "o"],
+               ["o", "o", "o", "o"],
+               ["o", "x", "o", "o"]]
 
 board.print()
+console.log(board.getWinner())
+//console.log(board.getWinnerFromRow(["x", "x", "x", "x"]))

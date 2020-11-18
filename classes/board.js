531ba490e11f517d0989e5f9a83107216d5da561
unknown <pierrick@live.co.uk>
2020-11-18 14:17:33 +0000

    Fixed antidiagonal testing

--- classes/board.js
+++ classes/board.js
@@ -1,9 +1,23 @@
 // Represents a game board
 class Board {
     constructor(width, height) {
-        this.width = width
-        this.height = height
+        // Remember that the board is indexed as this.state[y][x]
+        this.width = width // The number of cells in a row (the inner array)
+        this.height = height // The number of rows in the state (the outer array)
         this.clear()
+    }
+
+    getRow(y) {
+        return this.state[y]
+    }
+    
+    // Gets a single column and fills it into an array
+    getColumn(x) {
+        return this.state.map(row => row[x])
+    }
+
+    getCounter(x, y) {
+        return this.state[y][x]
     }
 
     // Returns winner if there is one
@@ -13,45 +27,34 @@ class Board {
         console.log("Diag win: " + this.getWinnerFromDiagonal())
         console.log("Antidiag win: " + this.getWinnerFromAntiDiagonal())
     }
+
+    // Returns true if every cell of the board is empty
+    isEmpty() {
+        return this.state.every(cell => !cell)
+    }
     
-    // Gets antidiagonal and fills it into an array
-    getAntiDiagonal() {
-        let antiDiagonal = []
-        for (let x = this.state.length, y = this.state.length; x > 0, y < 0; x--, y--) {
-            antiDiagonal.push(this.state[x][y])
-        }
-        return antiDiagonal
+    // Returns true if every cell of the board is full
+    isFull() {
+        return this.state.every(cell => cell)
     }
 
-    // Checks antidiagonal for a winner. Returns winner if there is one
-    getWinnerFromAntiDiagonal() {
-        const antiDiagonal = this.getAntiDiagonal()
-        const winner = this.getWinnerFromArray(antiDiagonal)
-        return winner
-    }
-    
-    // Gets diagonal and fills it into an array
-    getDiagonal() {
-        let diagonal = []
-        for (let x = 0, y = 0; x < this.state.length, y < this.state.length; x++, y++) {
-            diagonal.push(this.state[x][y])
-        }
-        return diagonal
+    // Clears board
+    clear() {
+        this.state = new Array(this.width).fill(new Array(this.height).fill(''))
     }
 
-    // Checks diagonal for a winner. Returns winner if there is one
-    getWinnerFromDiagonal() {
-        const diagonal = this.getDiagonal()
-        const winner = this.getWinnerFromArray(diagonal)
-        return winner
+    // Prints the board as rows and dividers
+    print() {
+        this.state.forEach((row, index) => {
+            this.printRow(row)
+            if (index < this.width - 1) {
+                this.printDivider()
+            }
+        })
+    }
 
-    }
-    
-    // Gets a single column and fills it into an array
-    getColumn(x) {
-        return this.state.map(row => row[x])
-    }
-    
+// ============================================ HELPER METHODS ============================================ //
+
     // Checks each column for a winner. Returns winner if there is one
     getWinnerFromColumns() {
         for (let x = 0; x < this.width; x++) {
@@ -67,13 +70,46 @@ class Board {
     // Checks each row for a winner. Returns the winner if there is one
     getWinnerFromRows() {
         for (let y = 0; y < this.height; y++) {
-            const row = this.state[y]
+            const row = this.getRow(y)
             const winner = this.getWinnerFromArray(row)
             if (winner != null) {
                 return winner
             }
         }
         return null
+    }
+
+    // Checks diagonal for a winner. Returns winner if there is one
+    getWinnerFromDiagonal() {
+        const diagonal = this.getDiagonal()
+        const winner = this.getWinnerFromArray(diagonal)
+        return winner
+
+    }
+
+    // Checks antidiagonal for a winner. Returns winner if there is one
+    getWinnerFromAntiDiagonal() {
+        const antiDiagonal = this.getAntiDiagonal()
+        const winner = this.getWinnerFromArray(antiDiagonal)
+        return winner
+    }
+    
+    // Gets diagonal and fills it into an array
+    getDiagonal() {
+        let diagonal = []
+        for (let x = 0, y = 0; x < this.width, y < this.width; x++, y++) {
+            diagonal.push(this.getCounter(x, y))
+        }
+        return diagonal
+    }
+
+    // Gets antidiagonal and fills it into an array
+    getAntiDiagonal() {
+        let antiDiagonal = []
+        for (let x = 0, y = this.height - 1; x > this.width; x++, y--) {
+            antiDiagonal.push(this.getCounter(x, y))
+        }
+        return antiDiagonal
     }
 
     // Returns winner from an array (x, o, null)
@@ -87,21 +123,6 @@ class Board {
             }
         }) 
         return winner
-    }
-    
-    // Clears board
-    clear() {
-        this.state = new Array(this.width).fill(new Array(this.height).fill(''))
-    }
-
-    // Prints the board as rows and dividers
-    print() {
-        this.state.forEach((row, index) => {
-            this.printRow(row)
-            if (index < this.width - 1) {
-                this.printDivider()
-            }
-        })
     }
 
     // Prints a row
@@ -125,16 +146,6 @@ class Board {
     printDivider() {
         const divider = '\u2015\u2015\u2015 '
         console.log(`${divider.repeat(this.width)}`)
-    }
-
-    // Returns true if every cell of the board is empty
-    isEmpty() {
-        return this.state.every(cell => !cell)
-    }
-    
-    // Returns true if every cell of the board is full
-    isFull() {
-        return this.state.every(cell => cell)
     }
 }
 
